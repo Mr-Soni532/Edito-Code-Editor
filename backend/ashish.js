dc3d788e51b267c37b5b188325a75b05c3e518b0
Ashish Pal <ashishpal1717@gmail.com>
2023-02-26 19:04:53 +0530

    Socket started

--- backend/ashish.js
+++ backend/ashish.js
@@ -1,83 +1,83 @@
-//Importing Modules
-const express =  require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-require("dotenv").config();
+// //Importing Modules
+// const express =  require("express");
+// const http = require("http");
+// const socketIo = require("socket.io");
+// require("dotenv").config();
 
-//Creating Server
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+// //Creating Server
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketIo(server);
 
-app.get("/", (req, res)=>{
-    res.send("Home");
-})
-
-
-// object for storing all the users with socket id
-const user_socket_map = {};
+// app.get("/", (req, res)=>{
+//     res.send("Home");
+// })
 
 
-// function for getting the connecting clients for a separate room
-function getAllConnectedClients(room_id) {
-    return Array.from(io.sockets.adapter.rooms.get(room_id) || []).map(
-        (socket_id) => {
-            return {
-                socket_id,
-                username: user_socket_map[socket_id],
-            };
-        }
-    );
-}
+// // object for storing all the users with socket id
+// const user_socket_map = {};
 
 
-// Connection made here
-io.on('connection', (socket) => {
-    console.log('socket connected', socket.id);
+// // function for getting the connecting clients for a separate room
+// function getAllConnectedClients(room_id) {
+//     return Array.from(io.sockets.adapter.rooms.get(room_id) || []).map(
+//         (socket_id) => {
+//             return {
+//                 socket_id,
+//                 username: user_socket_map[socket_id],
+//             };
+//         }
+//     );
+// }
 
 
-    socket.on("join", (room_id, username) => {
-        // storing users with unique socket id
-        user_socket_map[socket.id] = username;
-        // joining room 
-        socket.join(room_id);
-        // getting all the clients for the room
-        const clients = getAllConnectedClients(room_id);
-        clients.forEach(({ socketId }) => {
-            io.to(socketId).emit("join", {
-                clients,
-                username,
-                socketId: socket.id,
-            });
-        });
-    });
+// // Connection made here
+// io.on('connection', (socket) => {
+//     console.log('socket connected', socket.id);
 
-    // event for emitting the code_change 
-    socket.on("code_change", (room_id, code ) => {
-        socket.in(room_id).emit("code_change", { code });
-    });
 
-    // this event is syncing all the code
-    socket.on("sync_code", (socketId, code) => {
-        io.to(socketId).emit("code_change", { code });
-    });
+//     socket.on("join", (room_id, username) => {
+//         // storing users with unique socket id
+//         user_socket_map[socket.id] = username;
+//         // joining room 
+//         socket.join(room_id);
+//         // getting all the clients for the room
+//         const clients = getAllConnectedClients(room_id);
+//         clients.forEach(({ socketId }) => {
+//             io.to(socketId).emit("join", {
+//                 clients,
+//                 username,
+//                 socketId: socket.id,
+//             });
+//         });
+//     });
 
-    // disconnect event
-    socket.on('disconnecting', () => {
-        const rooms = [...socket.rooms];
-        rooms.forEach((room_id) => {
-            // emitting the 'disconnected event to frontend
-            socket.in(room_id).emit("disconnected", {
-                socketId: socket.id,
-                username: user_socket_map[socket.id],
-            });
-        });
-        // deleting the clientDetails after disconnecting
-        delete user_socket_map[socket.id];
-        socket.leave();
-    });
-});
+//     // event for emitting the code_change 
+//     socket.on("code_change", (room_id, code ) => {
+//         socket.in(room_id).emit("code_change", { code });
+//     });
 
-server.listen(process.env.port, ()=>{
-    console.log(`Serving at http://localhost:${process.env.port}`);
-})
+//     // this event is syncing all the code
+//     socket.on("sync_code", (socketId, code) => {
+//         io.to(socketId).emit("code_change", { code });
+//     });
+
+//     // disconnect event
+//     socket.on('disconnecting', () => {
+//         const rooms = [...socket.rooms];
+//         rooms.forEach((room_id) => {
+//             // emitting the 'disconnected event to frontend
+//             socket.in(room_id).emit("disconnected", {
+//                 socketId: socket.id,
+//                 username: user_socket_map[socket.id],
+//             });
+//         });
+//         // deleting the clientDetails after disconnecting
+//         delete user_socket_map[socket.id];
+//         socket.leave();
+//     });
+// });
+
+// server.listen(process.env.port, ()=>{
+//     console.log(`Serving at http://localhost:${process.env.port}`);
+// })
