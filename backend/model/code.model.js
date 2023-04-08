d610eb9cacc1d6671a5a6eefc018b5ef255aa71c
Bhupender Soni <official.soni532@gmail.com>
2023-04-09 04:47:43 +0530

    removed require true from the code schema

--- backend/model/code.model.js
+++ backend/model/code.model.js
@@ -1,25 +1,23 @@
 const mongoose = require('mongoose');
+
 const codeSchema = mongoose.Schema({
-    roomId: {
-        type: String,
-        required: true,
-      },
-      htmlCode: {
-        type: String,
-        required: true,
-      },
-      cssCode: {
-        type: String,
-        required: true,
-      },
-      jsCode: {
-        type: String,
-        required: true,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-      },
+  roomId: {
+    type: String,
+    required: true,
+  },
+  htmlCode: {
+    type: String,
+  },
+  cssCode: {
+    type: String,
+  },
+  jsCode: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 })
 const CodeModel = mongoose.model('code', codeSchema);
 module.exports = CodeModel;
