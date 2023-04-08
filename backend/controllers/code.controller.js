546fa0875bc57d390628d8dff8e7fcca2b857c2f
Bhupender Soni <official.soni532@gmail.com>
2023-04-08 21:23:09 +0530

    created code fetch handler and generate UUID handler

--- backend/controllers/code.controller.js
+++ backend/controllers/code.controller.js
@@ -1,7 +1,18 @@
 const CodeModel = require("../model/code.model");
+const { v4: uuidv4 } = require('uuid');
 
 exports.serverTesting = (req, res) => {
     res.send({ msg: "edito server working fine." });
+}
+
+exports.fetchCodeByRoomId = async (req, res) => {
+    const roomId = req.params.roomId;
+    try {
+        let roomCode = await CodeModel.findOne({ roomId })
+        res.send({ roomCode })
+    } catch (error) {
+        res.status(500).send({ msg: 'Something went wrong while fetching code.' })
+    }
 }
 
 exports.saveCode = async (req, res) => {
@@ -13,7 +24,7 @@ exports.saveCode = async (req, res) => {
             roomCode.cssCode = cssCode;
             roomCode.jsCode = jsCode;
             await roomCode.save();
-            res.status(201).send({msg: 'Code saved successfully.'})
+            res.status(201).send({ msg: 'Code saved successfully.' })
         } else {
             const newCode = new Code({
                 roomId,
@@ -22,9 +33,14 @@ exports.saveCode = async (req, res) => {
                 jsCode,
             });
             await newCode.save();
-            res.status(201).send({msg: 'Code saved successfully.'})
+            res.status(201).send({ msg: 'Code saved successfully.' })
         }
     } catch (error) {
-        res.status(500).send({msg: 'Something went wrong while saving code.'})
+        res.status(500).send({ msg: 'Something went wrong while saving code.' })
     }
+}
+
+exports.generateUUID = (req, res) => {
+    let id = uuidv4();
+    res.send({ id });
 }
